b0a0b8a9f37c71aaf208ec0c7f36d51b859cf51c
anshum12345 <anshumdwivedi8@gmail.com>
2024-06-08 14:06:21 +0530

    newone

--- src/subComponents/SocialIcons.js
+++ src/subComponents/SocialIcons.js
@@ -27,81 +27,50 @@ const Line = styled(motion.span)`
     props.color === "dark" ? DarkTheme.text : DarkTheme.body};
 `;
 
-const SocialIcons = (props) => {
+const SocialIcons = ({ theme }) => {
+  const iconColor = theme === "dark" ? DarkTheme.text : DarkTheme.body;
+
   return (
     <Icons>
-      <motion.div
-        whileHover={{ scale: 1.2 }}
-        whileTap={{ scale: 0.8 }}
-        transition={{ type: "spring", duration: 0.3 }}
-      >
-        <a
-          style={{ color: "inherit" }}
-          target="_blank"
-          href={"https://github.com/anshum12345"}
+      {[
+        {
+          href: "https://github.com/anshum12345",
+          Component: Github,
+        },
+        {
+          href: "https://www.linkedin.com/in/anshum-dwivedi-67530922a/",
+          Component: Linkedin,
+        },
+        {
+          href: "https://leetcode.com/Xavier_2212/",
+          Component: LeetCode,
+        },
+        {
+          href:
+            "https://drive.google.com/file/d/1V8aDX6tYoxaIfYrwIeuDiWh6Hk4ezXXr/view?usp=drivesdk",
+          Component: Resume,
+        },
+      ].map(({ href, Component }, index) => (
+        <motion.div
+          key={index}
+          whileHover={{ scale: 1.2 }}
+          whileTap={{ scale: 0.8 }}
+          transition={{ type: "spring", duration: 0.3 }}
         >
-          <Github
-            width={25}
-            height={25}
-            fill={props.theme === "dark" ? DarkTheme.text : DarkTheme.body}
-          />
-        </a>
-      </motion.div>
-      <motion.div
-        whileHover={{ scale: 1.2 }}
-        whileTap={{ scale: 0.8 }}
-        transition={{ type: "spring", duration: 0.3 }}
-      >
-        <a
-          style={{ color: "inherit" }}
-          target="_blank"
-          href={"https://www.linkedin.com/in/anshum-dwivedi-67530922a/"}
-        >
-          <Linkedin
-            width={25}
-            height={25}
-            fill={props.theme === "dark" ? DarkTheme.text : DarkTheme.body}
-          />
-        </a>
-      </motion.div>
-      <motion.div
-        whileHover={{ scale: 1.2 }}
-        whileTap={{ scale: 0.8 }}
-        transition={{ type: "spring", duration: 0.3 }}
-      >
-        <a
-          style={{ color: "inherit" }}
-          target="_blank"
-          href={"https://leetcode.com/Xavier_2212/"}
-        >
-          <LeetCode
-            width={25}
-            height={25}
-            fill={props.theme === "dark" ? DarkTheme.text : DarkTheme.body}
-          />
-        </a>
-      </motion.div>
-      <motion.div
-        whileHover={{ scale: 1.2 }}
-        whileTap={{ scale: 0.8 }}
-        transition={{ type: "spring", duration: 0.3 }}
-      >
-        <a
-          style={{ color: "inherit" }}
-          target="_blank"
-          href={"https://drive.google.com/file/d/16bjuWGUIdgAm9xfKP4M7b4DybpyoDMMF/view?usp=drivesdk"}
-        >
-          <Resume
-            width={25}
-            height={25}
-            fill={props.theme === "dark" ? DarkTheme.text : DarkTheme.body}
-          />
-        </a>
-      </motion.div>
+          <a
+            style={{ color: "inherit" }}
+            target="_blank"
+            rel="noopener noreferrer"
+            href={href}
+          >
+            <Component width={25} height={25} fill={iconColor} />
+          </a>
+        </motion.div>
+      ))}
       <Line
-        color={props.theme}
+        color={theme}
         initial={{ height: 0 }}
-        animate={{ height: "4rem" }}
+        animate={{ height: "8rem" }}
         transition={{ type: "spring", duration: 1, delay: 0.8 }}
       />
     </Icons>
